bf2b6067f4ec316046bf4b7f37bb11704bca7450
Tyler Holmes <tholmes@axian.com>
2023-09-24 13:17:12 -0700

    Adds, modes for Proximity, RFID. Adds .gitignore.

--- tinyhome/tinyhome.ino
+++ tinyhome/tinyhome.ino
@@ -2,6 +2,9 @@
 #include <StateMachine.h>
 #include <LiquidCrystal_I2C.h>
 #include <debounce.h>
+#include <ESP32_Servo.h>
+#include <Wire.h>
+#include <MFRC522_I2C.h>
 
 // Yellow Buttons (Menu/Interact)
 #define YELLOW_BUTTON_1_PIN 16
@@ -11,13 +14,19 @@
 #define WATER_SENSOR_PIN    34
 #define FAN_MOTOR_PMW_PIN   19
 #define FAN_SPEED_PIN       18
+#define PROXIMITY_PIN       14
+#define RFID_PIN            16
+#define WINDOW_SERVO_PIN    5
+#define DOOR_SERVO_PIN      13
 
 // State Machine States
-#define State0Home  0
-#define State1LED   1
-#define State2Temp  2
-#define State3Water 3
-#define State4Fan   4
+#define State0Home        0
+#define State1LED         1
+#define State2Temp        2
+#define State3Water       3
+#define State4Fan         4
+#define State5Proximity   5
+#define State6RfidOpen    6
 
 // LCD
 LiquidCrystal_I2C lcd(0x27,16,2);
@@ -31,8 +40,10 @@ State* S1 = machine.addState(&state1LED);
 State* S2 = machine.addState(&state2Temp);
 State* S3 = machine.addState(&state3Water);
 State* S4 = machine.addState(&state4Fan);
+State* S5 = machine.addState(&state5Proximity);
+State* S6 = machine.addState(&state6Rfid);
 
-// Define your button with a unique id (0) and handler function.
+// Button Handlers
 // (The ids are so one handler function can tell different buttons apart if necessary.)
 static Button yellow1(0, stateChange_Handler);
 static Button yellow2(1, action_Handler);
@@ -47,6 +58,60 @@ unsigned char dht[4] = {0, 0, 0, 0}; //Only the first 32 bits of data are receiv
 
 // Fan
 int fanMode = 0;
+
+// Door & Window
+bool doorOpen = false;
+bool windowOpen = false;
+Servo windowMotor;
+Servo doorMotor;
+
+// RFID Reader NOTE the -1 on the RST_PIN.
+// 0x28 is the i2c address of SDA, if doesn't match，please check your address with i2c. IIC pins default to GPIO21 and GPIO22 of ESP32
+MFRC522_I2C mfrc522(0x28, -1);
+String password = "";
+
+void setup() {
+  Serial.begin(115200);
+  
+  // LCD Setup
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  // I2C and Rfid
+  Wire.begin();                   // initialize I2C
+  mfrc522.PCD_Init();             // initialize MFRC522
+
+  // Servos
+  windowMotor.attach(WINDOW_SERVO_PIN);
+  doorMotor.attach(DOOR_SERVO_PIN);
+  windowMotor.write(0);
+  windowOpen = false;
+  doorMotor.write(0);
+  doorOpen = false;
+
+  // Button Setup
+  pinMode(YELLOW_BUTTON_1_PIN, INPUT_PULLUP);
+  pinMode(YELLOW_BUTTON_2_PIN, INPUT_PULLUP);
+  pinMode(LED_INDICATOR_PIN, OUTPUT);  //Set pin to output mode
+  pinMode(FAN_MOTOR_PMW_PIN, OUTPUT);
+  pinMode(FAN_SPEED_PIN, OUTPUT);
+  pinMode(PROXIMITY_PIN, INPUT);
+
+  // Start the state machine.
+  machine.run();
+}
+
+void loop() {
+    pollButtons(); // Poll buttons every loop.
+    delay(10); // Debounce iterations should run fairly quickly, 10's of ms, not 100's.
+    
+    // Exercise the state machine every ~STATE_DELAY or so.
+    if (millis() - lastRun >= STATE_DELAY) {
+      lastRun += STATE_DELAY;
+      machine.run();
+    }
+}
 
 // State transitions handled here. In general we:
 // 1) Run exit handlers for the state that's exiting.
@@ -72,13 +137,19 @@ static void stateChange_Handler(uint8_t btnId, uint8_t btnState) {
         machine.transitionTo(S4);
         break;
       case State4Fan:
+        machine.transitionTo(S5);
+        break;
+      case State5Proximity:
+        machine.transitionTo(S6);
+        break;
+      case State6RfidOpen:
         machine.transitionTo(S0);
         break;
       default:
         Serial.println("No state transition found!");
     }
 
-    exitState();
+    exitCurrentState();
     machine.run();
   }
 }
@@ -101,42 +172,18 @@ static void action_Handler(uint8_t btnId, uint8_t btnState) {
           fanMode = 0;
         }
         break;
+      case State6RfidOpen:
+        // If the door is open, and they press the Action button, close the door.
+        if (doorOpen) {
+          doorMotor.write(0);
+          doorOpen = false;
+        }
       default:
         Serial.println("No state transition found!");
     }
   }
 
   machine.run();
-}
-
-void setup() {
-  Serial.begin(115200);
-  
-  // LCD Setup
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-
-  // Button Setup
-  pinMode(YELLOW_BUTTON_1_PIN, INPUT_PULLUP);
-  pinMode(YELLOW_BUTTON_2_PIN, INPUT_PULLUP);
-  pinMode(LED_INDICATOR_PIN, OUTPUT);  //Set pin to output mode
-  pinMode(FAN_MOTOR_PMW_PIN, OUTPUT);
-  pinMode(FAN_SPEED_PIN, OUTPUT);
-
-  // Start the state machine.
-  machine.run();
-}
-
-void loop() {
-    pollButtons(); // Poll buttons every loop.
-    delay(10); // Debounce iterations should run fairly quickly, 10's of ms, not 100's.
-    
-    // Exercise the state machine ever 
-    if (millis() - lastRun >= STATE_DELAY) {
-      lastRun += STATE_DELAY;
-      machine.run();
-    }
 }
 
 static void pollButtons() {
@@ -233,7 +280,61 @@ void state4Fan() {
   }
 }
 
-void exitState() {
+void state5Proximity() {
+  if(machine.executeOnce){
+    lcd.print("Proximity");
+  }
+  lcd.setCursor(0, 1);
+
+  bool proximityFound = digitalRead(PROXIMITY_PIN);
+  if(proximityFound) {
+    lcd.print("Activity!     ");
+  }
+  else {
+    lcd.print("No Activity.  ");
+  }
+}
+
+void state6Rfid() {
+  if(machine.executeOnce){
+    lcd.print("RFID Door");
+  }
+  lcd.setCursor(0, 1);
+
+  // Rever LCD status
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    if (doorOpen) {
+      lcd.print("Open.       ");
+    } else {
+      lcd.print("Closed.     ");
+    }
+    password = "";
+    return;
+  }
+  
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    password = password + String(mfrc522.uid.uidByte[i]);
+  }
+  if(password == "693854118")  //The card number is correct, open the door
+  {
+    if (doorOpen) {
+      // Door already open.
+      lcd.print("Already Open");
+      delay(500);
+    } else {
+      // Open door
+      doorMotor.write(180);
+      doorOpen = true;
+    }
+  }
+  else   //The card number is wrong，LCD displays error
+  {
+    lcd.print("Wrong Key!  ");
+    delay(500);
+  }
+}
+
+void exitCurrentState() {
   Serial.println("Exit state: ");
   Serial.println(machine.currentState);
   lcd.clear();
